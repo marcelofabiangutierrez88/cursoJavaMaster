359570e9b1896b7881693cdd09d02948eddd4778
Marcelo Gutierrez <49662472+marcelofabiangutierrez88@users.noreply.github.com>
2019-07-16 22:48:16 -0300

    contador con el ciclo for
    Como en while y do while . se realiza un contador pero utilizando el ciclo for para conocer su sintaxis y funcionamiento

---
+++ UtilizandoFor/src/utilizandofor/UtilizandoFor.java
@@ -0,0 +1,15 @@
+
+package utilizandofor;
+
+
+public class UtilizandoFor {
+
+  
+    public static void main(String[] args) {
+        for (int i=0; i<=15; i++){
+            System.out.println ("Imprimiendo desde ciclo for :" +i);
+        }
+       
+    }
+    
+}
