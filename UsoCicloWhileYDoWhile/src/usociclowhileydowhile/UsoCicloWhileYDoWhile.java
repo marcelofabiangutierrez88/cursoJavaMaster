8975fe1c2f9fd466d22dd133f85c8cf7ebaae7aa
Marcelo Gutierrez <49662472+marcelofabiangutierrez88@users.noreply.github.com>
2019-07-16 22:41:51 -0300

    Utilizando ciclos while y do while
    Simple impresion de numeros utilizando ciclo while y do while utilizando contadores

---
+++ UsoCicloWhileYDoWhile/src/usociclowhileydowhile/UsoCicloWhileYDoWhile.java
@@ -0,0 +1,28 @@
+
+package usociclowhileydowhile;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class UsoCicloWhileYDoWhile {
+
+   
+    public static void main(String[] args) {
+        int i = 0 ;
+        
+        while ( i <=15){
+            System.out.println ("imprimiendo desde ciclo while:" +i);
+            i++;
+        }
+        
+        int c = 0;
+        
+        do {
+            System.out.println ("Imprimiendo desde DoWhile :" + c);
+            c++;
+        } while (c <=15);
+
+    }
+    
+}
