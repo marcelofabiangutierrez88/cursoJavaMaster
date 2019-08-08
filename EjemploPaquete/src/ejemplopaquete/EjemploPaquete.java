31bebca6c71bb80f840444ee3d3277af73be647f
Marcelo Gutierrez <49662472+marcelofabiangutierrez88@users.noreply.github.com>
2019-08-07 21:26:36 -0300

    Ejemplo paquetes
    Como trabajar con paquetes e import.

---
+++ EjemploPaquete/src/ejemplopaquete/EjemploPaquete.java
@@ -0,0 +1,19 @@
+
+package ejemplopaquete;
+import ejemplopaquete.marcelo.*;
+/**
+ *
+ * @author Marcelo
+ */
+public class EjemploPaquete {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Metodo imprimir desde la clase utileria :");
+        Utileria.Imprimir("Hola");
+        System.out.println("Metodo suma desde la clase utileria:");
+        Utileria.Suma(15, 20);
+        
+    }
+    
+}
---
+++ EjemploPaquete/src/ejemplopaquete/marcelo/Utileria.java
@@ -0,0 +1,16 @@
+
+package ejemplopaquete.marcelo;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class Utileria {
+    public static void Imprimir(String s){
+        System.out.println("Imprimir cadena"+s);
+    }
+    public static int Suma(int a , int b){
+       System.out.println("El resultado de la suma es :");
+       return a+b;
+    }
+}
